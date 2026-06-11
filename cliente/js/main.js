a0959164fda32c0137e29cee1e7cd76d59dcb6e6
Javier Farré Avilés <javifaa@gmail.com>
2026-06-11 14:12:54 +0200

    Validado el formulario.

--- cliente/js/main.js
+++ cliente/js/main.js
@@ -17,9 +17,9 @@ const listaCanciones = [
   { id: "0A", titulo: "Lost city of the resonant creations", dificultad: "2", notas: 355, estado: 0 },
   { id: "0B", titulo: "Valley of the dreaded", dificultad: "5", notas: 852 , estado: 0 },
   { id: "0C", titulo: "Nyarlathotep", dificultad: "6", notas: 897 , estado: 0 },
-  { id: "0D", titulo: "Woods of the endless party", dificultad: "3", estado: 0 },
-  { id: "0E", titulo: "Legends of the sacred dance", dificultad: "4", estado: 0 },
-  { id: "0F", titulo: "The old rites of Gradgordur", dificultad: "5", estado: 0 },
+  { id: "0D", titulo: "Woods of the endless party", dificultad: "3", notas: 377, estado: 0 },
+  { id: "0E", titulo: "Legends of the sacred dance", dificultad: "4", notas: 488, estado: 0 },
+  { id: "0F", titulo: "The old rites of Gradgordur", dificultad: "5", notas: 561, estado: 0 },
 ];
 
 const listaPuntuaciones = [];
@@ -44,33 +44,33 @@ const formulario = document.getElementById("formulario");
 // FALTA EVENTO DE CAMBIO DE VISTA
 // Tambien falta una vista con las 10 canciones con las puntuaciones más altas.
 
-const valoresPuntuaciones = [
-  { id: "01", perfect: 150, good: 0, fail: 0 },
-  { id: "01", perfect: 125, good: 25, fail: 0 },
-  { id: "02", perfect: 520, good: 60, fail: 20 },
-  { id: "02", perfect: 540, good: 30, fail: 10 },
-  { id: "03", perfect: 355, good: 40, fail: 5 },
-  { id: "03", perfect: 320, good: 70, fail: 10 },
-  { id: "04", perfect: 425, good: 20, fail: 5 },
-  { id: "04", perfect: 405, good: 45, fail: 0 },
-  { id: "05", perfect: 40, good: 10, fail: 0 },
-  { id: "05", perfect: 49, good: 1, fail: 0 },
-  { id: "06", perfect: 620, good: 150, fail: 30 },
-  { id: "06", perfect: 620, good: 170, fail: 10 },
-  { id: "07", perfect: 620, good: 130, fail: 20 },
-  { id: "07", perfect: 690, good: 70, fail: 10 },
-  { id: "08", perfect: 465, good: 56, fail: 0 },
-  { id: "08", perfect: 480, good: 40, fail: 1 },
-  { id: "09", perfect: 565, good: 50, fail: 24 },
-  { id: "09", perfect: 550, good: 60, fail: 29 },
-  { id: "0A", perfect: 320, good: 30, fail: 5 },
-  { id: "0A", perfect: 340, good: 14, fail: 1 },
-  { id: "0B", perfect: 750, good: 60, fail: 42 },
-  { id: "0B", perfect: 716, good: 69, fail: 67 },
-  { id: "0C", perfect: 700, good: 107, fail: 90 },
-  { id: "0C", perfect: 680, good: 107, fail: 110 },
-];
 // Descomentar para añadir datos de forma automatica.
+// const valoresPuntuaciones = [
+//   { id: "01", perfect: 150, good: 0, fail: 0 },
+//   { id: "01", perfect: 125, good: 25, fail: 0 },
+//   { id: "02", perfect: 520, good: 60, fail: 20 },
+//   { id: "02", perfect: 540, good: 30, fail: 10 },
+//   { id: "03", perfect: 355, good: 40, fail: 5 },
+//   { id: "03", perfect: 320, good: 70, fail: 10 },
+//   { id: "04", perfect: 425, good: 20, fail: 5 },
+//   { id: "04", perfect: 405, good: 45, fail: 0 },
+//   { id: "05", perfect: 40, good: 10, fail: 0 },
+//   { id: "05", perfect: 49, good: 1, fail: 0 },
+//   { id: "06", perfect: 620, good: 150, fail: 30 },
+//   { id: "06", perfect: 620, good: 170, fail: 10 },
+//   { id: "07", perfect: 620, good: 130, fail: 20 },
+//   { id: "07", perfect: 690, good: 70, fail: 10 },
+//   { id: "08", perfect: 465, good: 56, fail: 0 },
+//   { id: "08", perfect: 480, good: 40, fail: 1 },
+//   { id: "09", perfect: 565, good: 50, fail: 24 },
+//   { id: "09", perfect: 550, good: 60, fail: 29 },
+//   { id: "0A", perfect: 320, good: 30, fail: 5 },
+//   { id: "0A", perfect: 340, good: 14, fail: 1 },
+//   { id: "0B", perfect: 750, good: 60, fail: 42 },
+//   { id: "0B", perfect: 716, good: 69, fail: 67 },
+//   { id: "0C", perfect: 700, good: 107, fail: 90 },
+//   { id: "0C", perfect: 680, good: 107, fail: 110 },
+// ];
 // for (let i = valoresPuntuaciones.length - 1; i >= 0; i--) {
 //   if (i > 0) {
 //     const j = Math.floor(Math.random() * (i + 1));
@@ -99,11 +99,17 @@ console.log(listaPuntuaciones);
 // FALTA VALIDAR LOS DATOS
 // En este caso, asegurarse de que hay una canción seleccionada, y de que se ha introducido el numero correcto de notas.
 
-function añadir() {
+function validar() {
   const cancion = listaCanciones.find(({ id }) => id === document.getElementById("canciones").value);
   const perfecto = Number(document.getElementById("perfecto").value);
   const bien = Number(document.getElementById("bien").value);
   const fallos = Number(document.getElementById("fallo").value);
+  const validado = cantidadesValidas(perfecto, bien, fallos, cancion.notas);
+  if (validado) añadir(cancion, perfecto, bien, fallos);
+  return validado;
+}
+
+function añadir(cancion, perfecto, bien, fallos) {
   const puntuaciónActual = puntuación(perfecto, bien, cancion.notas);
   const estadoPuntuacion = estado(bien, fallos);
   añadePuntuaciones(listaPuntuaciones, listaCanciones, cancion.id, puntuaciónActual, estadoPuntuacion);
@@ -116,4 +122,4 @@ function añadir() {
   sessionStorage.setItem("mejoresEstados",JSON.stringify(mejoresEstados));
 }
 
-formulario.addEventListener("submit", añadir);
+formulario.addEventListener("submit", validar);
